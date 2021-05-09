c86e64cc93328fc3b07324bb5694311c4514445d
devkant <dev.chouhan9213@gmail.com>
2021-05-09 11:54:11 +0530

    remove unused node_modules

---
+++ solution.js
@@ -0,0 +1,59 @@
+const days = {
+    0 : "Sun",
+    1 : "Mon",
+    2 : "Tue",
+    3 : "Wed",
+    4 : "Thu",
+    5 : "Fri",
+    6 : "Sat"
+}
+
+function solution(D) {
+    let output = {};
+    for(let k of Object.keys(D)) {
+        let date = new Date(k);
+        let currDay = date.getDay();
+        if(output.hasOwnProperty(days[currDay])) {
+            output[days[currDay]] = output[days[currDay]] + D[k];
+        } else {
+            output[days[currDay]] = D[k];
+        }
+    }
+
+    
+    for(let i = 0; i < 7; i++) {
+        if(!output.hasOwnProperty(days[i])) {
+            let prev = prevDay(i, output);
+            let nxt = nextDay(i, output);
+            let n;
+            if((nxt -  i > 0)) {
+                n = nxt - i;
+            } else {
+                n = 7 + nxt - i;
+            }
+            output[days[i]] = (n * (output[days[prev]]) + output[days[nxt]]) / (n+1);
+        } else {
+            output[days[i]] = output[days[i]];
+        }
+    }
+
+    return output;
+}
+
+function prevDay(currDay, output) {
+    for(let j = currDay - 1; j >= 0; j++) {
+        if(output.hasOwnProperty(days[j])) {
+            return j;
+        }
+    }
+}
+
+function nextDay(currDay, output) {
+    for(let j = currDay + 1; ; j = (j+1) % 7 ) {
+        if(output.hasOwnProperty(days[j])) {
+            return j;
+        }
+    }
+}
+
+module.exports =  {solution, prevDay, nextDay, days};
---
+++ test.js
@@ -0,0 +1,13 @@
+const {solution, prevDay, nextDay, days} = require('./solution');
+
+const assert = require('assert');
+
+it('Case when two days are missing in dictionary', () => {
+    assert.deepEqual(solution({"2020-01-01": 6,"2020-01-04": 12,"2020-01-05": 14,"2020-01-06": 2,
+    "2020-01-07": 4}), { Wed: 6, Sat: 12, Sun: 14, Mon: 2, Tue: 4, Thu: 8, Fri: 10 })
+});
+
+it('Case when no day is missing in dictionary', () => {
+    assert.deepEqual(solution({"2020-01-01": 4,"2020-01-02": 4,"2020-01-03": 6, "2020-01-04": 8, "2020-01-05": 2,
+    "2020-01-06": -6, "2020-01-07": 2, "2020-01-08": -2}), {'Mon':-6, 'Tue':2, 'Wed':2, 'Thu':4, 'Fri':6, 'Sat':8, 'Sun':2});
+})
